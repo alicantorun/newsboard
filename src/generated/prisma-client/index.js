b5626b3392f61adb26e790441d0b0d0af2892fc1
Alican Torun <torun.alican@gmail.com>
2019-08-06 18:14:43 +0200

    prisma generated fix

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/alican-torun-aeda92/newsboard/dev`
+});
+exports.prisma = new exports.Prisma();
