195228bb65d1b8b1d2006d1f0445403ce16a0781
Alican Torun <torun.alican@gmail.com>
2019-08-08 22:17:25 +0200

    pre-auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import logo from "../logo.svg";
 import "../styles/App.css";
+import { Switch, Route } from "react-router-dom";
 import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
-    return <CreateLink />;
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
