72a60d2ce43daea55a4675ae5d20d0ee12f66337
Alican Torun <torun.alican@gmail.com>
2019-08-08 21:55:39 +0200

    pre routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import logo from "../logo.svg";
 import "../styles/App.css";
 import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
 class App extends React.Component {
   render() {
-    return <LinkList />;
+    return <CreateLink />;
   }
 }
 
